792828365d6ce5692130d0dd4f2e313dabd2e2d3
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-07-16 14:40:45 -0300

    fish: fix indentation

--- fish/install.sh
+++ fish/install.sh
@@ -3,8 +3,8 @@
 set -e
 
 if ! command -v fish > /dev/null; then
-	echo Installing fish
-	pacman -S fish
+  echo Installing fish
+  pacman -S fish
 
   echo Installing omf
   curl -L https://get.oh-my.fish | fish
