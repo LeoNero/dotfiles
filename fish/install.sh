a4314ee9756f47d0e28cbc81218382c36bb6cac4
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-07-16 04:02:08 -0300

    chore: add fish files

---
+++ fish/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+if ! command -v fish > /dev/null; then
+	echo Installing fish
+	pacman -S fish
+
+  echo Installing omf
+  curl -L https://get.oh-my.fish | fish
+fi
+
+echo Linking files
+ln -s ~/.dotfiles/fish/fish ~/.config/fish
+ln -s ~/.dotfiles/fish/omf ~/.config/omf
